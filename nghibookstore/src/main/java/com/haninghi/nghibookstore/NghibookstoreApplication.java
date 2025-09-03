361702d230f7ce6f6c2b028b37c5cbda318325e3
NghiVo <vmmn27@gmail.com>
2025-09-03 11:43:58 +0300

    Set up connection with H2 db

--- nghibookstore/src/main/java/com/haninghi/nghibookstore/NghibookstoreApplication.java
+++ nghibookstore/src/main/java/com/haninghi/nghibookstore/NghibookstoreApplication.java
@@ -1,7 +1,12 @@
 package com.haninghi.nghibookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.haninghi.nghibookstore.domain.Book;
+import com.haninghi.nghibookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class NghibookstoreApplication {
@@ -10,4 +15,12 @@ public class NghibookstoreApplication {
 		SpringApplication.run(NghibookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(BookRepository bookRepository) {
+		return (args) -> {
+			bookRepository.save(
+					new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 1979, "0-345-39180-2", 15.99));
+			bookRepository.save(new Book("Dune", "Frank Herbert", 1965, "978-0441172719", 22.50));
+		};
+	}
 }
---
+++ nghibookstore/src/main/java/com/haninghi/nghibookstore/domain/BookRepository.java
@@ -0,0 +1,10 @@
+package com.haninghi.nghibookstore.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface BookRepository extends CrudRepository <Book,Long> {
+    List<Book> findByAuthor(String author);
+}

76d4d9d7f47a3e979c3b76cbfee89804be5f87eb
NghiVo <vmmn27@gmail.com>
2025-09-05 16:44:43 +0300

    Create add and delete features

--- nghibookstore/src/main/java/com/haninghi/nghibookstore/domain/Book.java
+++ nghibookstore/src/main/java/com/haninghi/nghibookstore/domain/Book.java
@@ -18,7 +18,8 @@ public class Book {
     private String isbn;
     private double price;
 
-    public Book() {}
+    public Book() {
+    }
 
     public Book(String title, String author, int publicationYear, String isbn, double price) {
         this.title = title;
@@ -28,36 +29,52 @@ public class Book {
         this.price = price;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getAuthor() {
         return author;
     }
+
     public void setAuthor(String author) {
         this.author = author;
     }
+
     public int getPublicationYear() {
         return publicationYear;
     }
+
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
     }
+
     public String getIsbn() {
         return isbn;
     }
+
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
+
     public double getPrice() {
         return price;
     }
+
     public void setPrice(double price) {
         this.price = price;
     }
-
 
 }
--- nghibookstore/src/main/java/com/haninghi/nghibookstore/web/BookController.java
+++ nghibookstore/src/main/java/com/haninghi/nghibookstore/web/BookController.java
@@ -4,31 +4,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.haninghi.nghibookstore.domain.Book;
 import com.haninghi.nghibookstore.domain.BookRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BookController {
     @Autowired
     private BookRepository repository;
 
-    public BookController(BookRepository repository){
+    public BookController(BookRepository repository) {
         this.repository = repository;
     }
-
 
     @GetMapping("/")
     public String getMainScreen() {
         return "index";
     }
 
-    @RequestMapping(value = {"/", "/booklist"})
+    @RequestMapping(value = { "/", "/booklist" })
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
 
+    @RequestMapping(value = { "/addbook" })
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @RequestMapping(value = { "/save" }, method = RequestMethod.POST)
+    public String save(Book book) {
+        repository.save(book);
+        return "redirect:booklist";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
 
 }
